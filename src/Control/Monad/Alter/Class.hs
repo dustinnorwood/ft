583f15e12e6d0c5e62821bde857c4f50150c4569
dustinnorwood <dnorwood2010@aol.com>
2019-02-10 17:17:57 -0500

    Remove Monad constraint from update and modify

--- src/Control/Monad/Alter/Class.hs
+++ src/Control/Monad/Alter/Class.hs
@@ -1,10 +1,10 @@
+{-# LANGUAGE LambdaCase             #-}
 {-# LANGUAGE MultiParamTypeClasses  #-}
 {-# LANGUAGE TypeOperators          #-}
 
 module Control.Monad.Alter.Class
   ( Alters(..)
   , X(..)
-  , ffor
   , update
   , updateStatefully
   , modify
@@ -15,33 +15,31 @@ module Control.Monad.Alter.Class
   , get
   , alter_
   , update_
-  , updateState_
+  , updateStatefully_
   , modify_
-  , modifyState_
+  , modifyStatefully_
   , repsert_
   ) where
 
-import Control.Monad             (join, void)
+import Control.Monad             (void)
 import Control.Monad.Trans.State (evalStateT, execStateT, StateT)
 import Data.Maybe
-import Data.Traversable          (for)
 
 data X a = P
-
-ffor :: (Applicative f, Monad t, Traversable t) => t a -> (a -> f (t b)) -> f (t b)
-ffor t = fmap join . for t
 
 class Applicative f => Alters k a f where
   alter :: X a -> k -> (Maybe a -> f (Maybe a)) -> f (Maybe a)
   {-# MINIMAL alter #-}
 
-update :: (Monad m, (k `Alters` a) m) => X a -> k -> (a -> m (Maybe a)) -> m (Maybe a)
-update p k = alter p k . flip ffor
+update :: (k `Alters` a) m => X a -> k -> (a -> m (Maybe a)) -> m (Maybe a)
+update p k f = alter p k $ \case
+  Just a -> f a
+  Nothing -> pure Nothing
 
 updateStatefully :: (Monad m, (k `Alters` a) m) => X a -> k -> StateT a m (Maybe a) -> m (Maybe a)
 updateStatefully p k = update p k . evalStateT
 
-modify :: (Monad m, (k `Alters` a) m) => X a -> k -> (a -> m a) -> m a
+modify :: (k `Alters` a) m => X a -> k -> (a -> m a) -> m a
 modify p k f = fmap fromJust $ update p k (fmap Just . f)
 
 modifyStatefully :: (Monad m, (k `Alters` a) m) => X a -> k -> StateT a m () -> m a
@@ -62,17 +60,17 @@ get p k = alter p k pure
 alter_ :: (k `Alters` a) f => X a -> k -> (Maybe a -> f (Maybe a)) -> f ()
 alter_ p k = void . alter p k
 
-update_ :: (Monad m, (k `Alters` a) m) => X a -> k -> (a -> m (Maybe a)) -> m ()
+update_ :: (k `Alters` a) m => X a -> k -> (a -> m (Maybe a)) -> m ()
 update_ p k = void . update p k
 
-updateState_ :: (Monad m, (k `Alters` a) m) => X a -> k -> StateT a m (Maybe a) -> m ()
-updateState_ p k = void . updateStatefully p k
+updateStatefully_ :: (Monad m, (k `Alters` a) m) => X a -> k -> StateT a m (Maybe a) -> m ()
+updateStatefully_ p k = void . updateStatefully p k
 
-modify_ :: (Monad m, (k `Alters` a) m) => X a -> k -> (a -> m a) -> m ()
+modify_ :: (k `Alters` a) m => X a -> k -> (a -> m a) -> m ()
 modify_ p k = void . modify p k
 
-modifyState_ :: (Monad m, (k `Alters` a) m) => X a -> k -> StateT a m () -> m ()
-modifyState_ p k = void . modifyStatefully p k
+modifyStatefully_ :: (Monad m, (k `Alters` a) m) => X a -> k -> StateT a m () -> m ()
+modifyStatefully_ p k = void . modifyStatefully p k
 
 repsert_ :: (k `Alters` a) f => X a -> k -> (Maybe a -> f a) -> f ()
 repsert_ p k = void . repsert p k
