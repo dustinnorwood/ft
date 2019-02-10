70c2cb131c08a101df820ba0a11e7462257de152
dustinnorwood <dnorwood2010@aol.com>
2019-02-10 16:39:15 -0500

    Use extra class parameters rather than type family

--- src/Control/Monad/Alter/Class.hs
+++ src/Control/Monad/Alter/Class.hs
@@ -1,11 +1,24 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE TypeOperators          #-}
 
 module Control.Monad.Alter.Class
-    ( Alters(..)
-    , X(..)
-    , ffor
-    ) where
+  ( Alters(..)
+  , X(..)
+  , ffor
+  , update
+  , updateStatefully
+  , modify
+  , modifyStatefully
+  , repsert
+  , insert
+  , get
+  , alter_
+  , update_
+  , updateState_
+  , modify_
+  , modifyState_
+  , repsert_
+  ) where
 
 import Control.Monad             (join, void)
 import Control.Monad.Trans.State (evalStateT, execStateT, StateT)
@@ -17,45 +30,45 @@ data X a = P
 ffor :: (Applicative f, Monad t, Traversable t) => t a -> (a -> f (t b)) -> f (t b)
 ffor t = fmap join . for t
 
-class Monad m => Alters k a m where
+class Alters k a m where
   alter :: X a -> k -> (Maybe a -> m (Maybe a)) -> m (Maybe a)
   {-# MINIMAL alter #-}
 
-  update :: X a -> k -> (a -> m (Maybe a)) -> m (Maybe a)
-  update p k = alter p k . flip ffor
+update :: (Monad m, (k `Alters` a) m) => X a -> k -> (a -> m (Maybe a)) -> m (Maybe a)
+update p k = alter p k . flip ffor
 
-  updateState :: X a -> k -> StateT a m (Maybe a) -> m (Maybe a)
-  updateState p k = update p k . evalStateT
+updateStatefully :: (Monad m, (k `Alters` a) m) => X a -> k -> StateT a m (Maybe a) -> m (Maybe a)
+updateStatefully p k = update p k . evalStateT
 
-  modify :: X a -> k -> (a -> m a) -> m a
-  modify p k f = fmap fromJust $ update p k (fmap Just . f)
+modify :: (Monad m, (k `Alters` a) m) => X a -> k -> (a -> m a) -> m a
+modify p k f = fmap fromJust $ update p k (fmap Just . f)
 
-  modifyState :: X a -> k -> StateT a m () -> m a
-  modifyState p k = modify p k . execStateT
+modifyStatefully :: (Monad m, (k `Alters` a) m) => X a -> k -> StateT a m () -> m a
+modifyStatefully p k = modify p k . execStateT
 
-  repsert :: X a -> k -> (Maybe a -> m a) -> m a
-  repsert p k f = fmap fromJust $ alter p k (fmap Just . f)
+repsert :: (Functor m, (k `Alters` a) m) => X a -> k -> (Maybe a -> m a) -> m a
+repsert p k f = fmap fromJust $ alter p k (fmap Just . f)
 
-  insert :: X a -> k -> a -> m ()
-  insert p k a = alter_ p k (return . const (Just a))
+insert :: (Applicative m, (k `Alters` a) m) => X a -> k -> a -> m ()
+insert p k a = alter_ p k (pure . const (Just a))
 
-  get :: X a -> k -> m (Maybe a)
-  get p k = alter p k return
+get :: (Applicative m, (k `Alters` a) m) => X a -> k -> m (Maybe a)
+get p k = alter p k pure
 
-  alter_ :: X a -> k -> (Maybe a -> m (Maybe a)) -> m ()
-  alter_ p k = void . alter p k
+alter_ :: (Functor m, (k `Alters` a) m) => X a -> k -> (Maybe a -> m (Maybe a)) -> m ()
+alter_ p k = void . alter p k
 
-  update_ :: X a -> k -> (a -> m (Maybe a)) -> m ()
-  update_ p k = void . update p k
+update_ :: (Monad m, (k `Alters` a) m) => X a -> k -> (a -> m (Maybe a)) -> m ()
+update_ p k = void . update p k
 
-  updateState_ :: X a -> k -> StateT a m (Maybe a) -> m ()
-  updateState_ p k = void . updateState p k
+updateState_ :: (Monad m, (k `Alters` a) m) => X a -> k -> StateT a m (Maybe a) -> m ()
+updateState_ p k = void . updateStatefully p k
 
-  modify_ :: X a -> k -> (a -> m a) -> m ()
-  modify_ p k = void . modify p k
+modify_ :: (Monad m, (k `Alters` a) m) => X a -> k -> (a -> m a) -> m ()
+modify_ p k = void . modify p k
 
-  modifyState_ :: X a -> k -> StateT a m () -> m ()
-  modifyState_ p k = void . modifyState p k
+modifyState_ :: (Monad m, (k `Alters` a) m) => X a -> k -> StateT a m () -> m ()
+modifyState_ p k = void . modifyStatefully p k
 
-  repsert_ :: X a -> k -> (Maybe a -> m a) -> m ()
-  repsert_ p k = void . repsert p k
+repsert_ :: (Functor m, (k `Alters` a) m) => X a -> k -> (Maybe a -> m a) -> m ()
+repsert_ p k = void . repsert p k
