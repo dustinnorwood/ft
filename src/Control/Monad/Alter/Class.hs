e3bce82596f879872d57d65010aaff42fc80d3ac
dustinnorwood <dnorwood2010@aol.com>
2019-02-11 14:13:05 -0500

    Add Modifies class; rename `modify` to `adjust`

--- src/Control/Monad/Alter/Class.hs
+++ src/Control/Monad/Alter/Class.hs
@@ -1,24 +1,28 @@
-{-# LANGUAGE LambdaCase             #-}
-{-# LANGUAGE MultiParamTypeClasses  #-}
-{-# LANGUAGE TypeOperators          #-}
+{-# LANGUAGE ConstraintKinds       #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE LambdaCase            #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeOperators         #-}
 
 module Control.Monad.Alter.Class
-  ( Alters(..)
+  ( Modifies(..)
   , X(..)
-  , update
-  , updateStatefully
-  , modify
-  , modifyStatefully
-  , repsert
-  , insert
-  , delete
-  , get
+  , alter
   , alter_
+  , update
   , update_
   , updateStatefully_
-  , modify_
-  , modifyStatefully_
+  , updateStatefully
+  , adjust
+  , adjust_
+  , adjustStatefully
+  , adjustStatefully_
+  , repsert
   , repsert_
+  , get
+  , insert
+  , delete
   ) where
 
 import Control.Monad             (void)
@@ -27,50 +31,54 @@ import Data.Maybe
 
 data X a = P
 
-class Applicative f => Alters k a f where
-  alter :: X a -> k -> (Maybe a -> f (Maybe a)) -> f (Maybe a)
-  {-# MINIMAL alter #-}
+class Applicative f => Modifies k a f where
+  modify :: X a -> k -> (a -> f a) -> f a
 
-update :: (k `Alters` a) m => X a -> k -> (a -> m (Maybe a)) -> m (Maybe a)
+type Alters k a f = Modifies k (Maybe a) f
+
+alter :: (k `Alters` a) f => X a -> k -> (Maybe a -> f (Maybe a)) -> f (Maybe a)
+alter (P::X a) = modify (P::X (Maybe a))
+
+alter_ :: (k `Alters` a) f => X a -> k -> (Maybe a -> f (Maybe a)) -> f ()
+alter_ p k = void . alter p k
+
+update :: (k `Alters` a) f => X a -> k -> (a -> f (Maybe a)) -> f (Maybe a)
 update p k f = alter p k $ \case
   Just a -> f a
   Nothing -> pure Nothing
 
+update_ :: (k `Alters` a) f => X a -> k -> (a -> f (Maybe a)) -> f ()
+update_ p k = void . update p k
+
 updateStatefully :: (Monad m, (k `Alters` a) m) => X a -> k -> StateT a m (Maybe a) -> m (Maybe a)
 updateStatefully p k = update p k . evalStateT
 
-modify :: (k `Alters` a) m => X a -> k -> (a -> m a) -> m a
-modify p k f = fmap fromJust $ update p k (fmap Just . f)
+updateStatefully_ :: (Monad m, (k `Alters` a) m) => X a -> k -> StateT a m (Maybe a) -> m ()
+updateStatefully_ p k = void . updateStatefully p k
 
-modifyStatefully :: (Monad m, (k `Alters` a) m) => X a -> k -> StateT a m () -> m a
-modifyStatefully p k = modify p k . execStateT
+adjust :: (k `Alters` a) m => X a -> k -> (a -> m a) -> m a
+adjust p k f = fmap fromJust $ update p k (fmap Just . f)
+
+adjust_ :: (k `Alters` a) f => X a -> k -> (a -> f a) -> f ()
+adjust_ p k = void . adjust p k
+
+adjustStatefully :: (Monad m, (k `Alters` a) m) => X a -> k -> StateT a m () -> m a
+adjustStatefully p k = adjust p k . execStateT
+
+adjustStatefully_ :: (Monad m, (k `Alters` a) m) => X a -> k -> StateT a m () -> m ()
+adjustStatefully_ p k = void . adjustStatefully p k
 
 repsert :: (k `Alters` a) f => X a -> k -> (Maybe a -> f a) -> f a
 repsert p k f = fmap fromJust $ alter p k (fmap Just . f)
+
+repsert_ :: (k `Alters` a) f => X a -> k -> (Maybe a -> f a) -> f ()
+repsert_ p k = void . repsert p k
+
+get :: (k `Alters` a) f => X a -> k -> f (Maybe a)
+get p k = alter p k pure
 
 insert :: (k `Alters` a) f => X a -> k -> a -> f ()
 insert p k a = alter_ p k (pure . const (Just a))
 
 delete :: (k `Alters` a) f => X a -> k -> f ()
 delete p k = alter_ p k (pure . const Nothing)
-
-get :: (k `Alters` a) f => X a -> k -> f (Maybe a)
-get p k = alter p k pure
-
-alter_ :: (k `Alters` a) f => X a -> k -> (Maybe a -> f (Maybe a)) -> f ()
-alter_ p k = void . alter p k
-
-update_ :: (k `Alters` a) m => X a -> k -> (a -> m (Maybe a)) -> m ()
-update_ p k = void . update p k
-
-updateStatefully_ :: (Monad m, (k `Alters` a) m) => X a -> k -> StateT a m (Maybe a) -> m ()
-updateStatefully_ p k = void . updateStatefully p k
-
-modify_ :: (k `Alters` a) m => X a -> k -> (a -> m a) -> m ()
-modify_ p k = void . modify p k
-
-modifyStatefully_ :: (Monad m, (k `Alters` a) m) => X a -> k -> StateT a m () -> m ()
-modifyStatefully_ p k = void . modifyStatefully p k
-
-repsert_ :: (k `Alters` a) f => X a -> k -> (Maybe a -> f a) -> f ()
-repsert_ p k = void . repsert p k
