238ffac395164a4a098a1321805562aef204e21d
dustinnorwood <dnorwood2010@aol.com>
2019-02-28 10:38:34 -0500

    Remove reference to Proxy in Change.hs

--- src/Control/Monad/Change.hs
+++ src/Control/Monad/Change.hs
@@ -1,10 +1,8 @@
 module Control.Monad.Change
-  ( X(..)
-  , Modifiable(..)
+  ( Modifiable(..)
   , Alters(..)
   ) where
 
 import Control.Monad.Change.Alter
 import Control.Monad.Change.Modify
-import Control.Monad.Change.Proxy
 import Prelude                     hiding (lookup)
